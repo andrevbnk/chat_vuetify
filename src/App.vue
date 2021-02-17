b7325de37e7e0806cc5a153b47f94d666ec2c9f7
Andrey <andrevbnk72@gmail.com>
2021-02-17 23:31:52 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="dark"
+      dark
+    >
+
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col md="6" offset-md="3" >
+          <!-- <Chat /> -->CHAT
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data:()=>{
+      return{
+
+      }
+    }
+  }
+</script>
